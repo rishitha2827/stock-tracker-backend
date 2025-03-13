ec83182ec99d8c310fe0b92fd09eadfc2013ce3f
Bhaswanth Polamarasetty <bhaswanth0526@gmail.com>
2025-03-13 16:38:22 +0530

    added google passkey to .env and solved the issue related to price quote promise

--- jobs/stockMonitor.js
+++ jobs/stockMonitor.js
@@ -5,15 +5,23 @@ const { sendEmail } = require('../services/emailService');
 
 cron.schedule('*/1 * * * *', async () => {
   const alerts = await StockAlert.find({ status: 'active' });
+  if (alerts.length == 0) {
+    console.log('No alerts');
+  }
   for (const alert of alerts) {
     console.log(alert.symbol);
-    const price = getStockPrice(alert.symbol);
-    console.log(price);
-    if (price >= alert.targetPrice) {
-      await sendEmail(alert.email, 'Stock Alert', `${alert.symbol} has reached ${price}`);
-      alert.status = 'notified';
-      alert.lastNotifiedAt = new Date();
-      await alert.save();
+    try {
+      const price = await getStockPrice(alert.symbol);
+      console.log(price);
+      if (price >= alert.targetPrice) {
+        await sendEmail(alert.email, 'Stock Alert', `${alert.symbol} has reached ${price}`);
+        //alert.status = 'notified';
+        alert.lastNotifiedAt = new Date();
+        console.log('hello');
+        await alert.save();
+      }
+    } catch (error) {
+      console.error(`Error fetching stock price for ${alert.symbol}:  ${error}`)
     }
   }
 });
--- services/stockService.js
+++ services/stockService.js
@@ -4,10 +4,13 @@ const api_key = finnhub.ApiClient.instance.authentications['api_key'];
 api_key.apiKey = process.env.STOCK_API_KEY;
 const finnhubClient = new finnhub.DefaultApi();
 
-exports.getStockPrice = async (symbol) => {
-  var stockPrice;
-  finnhubClient.quote(symbol, (error, data, response) => {
-    stockPrice = data.c;
+exports.getStockPrice = (symbol) => {
+  return new Promise((resolve, reject) => {
+    finnhubClient.quote(symbol, (error, data, response) => {
+      if (error) {
+        return reject(error);
+      }
+      resolve(data.c);
+    });
   });
-  return stockPrice;
 };
